d0209f5c3abf63b4c9b544f28827c508e864a07a
MiguelNavarrete-dev <miguel.navarrete.dev@gmail.com>
2026-01-07 15:32:26 -0300

    termino de navidad

--- public/script.js
+++ public/script.js
@@ -1,9 +1,2 @@
 // cuando accedamos a la pagina y se cargue, se mostrara el año en el que estamos
 document.getElementById('year').textContent = new Date().getFullYear();
-
-SnowTheme.config = {
-    particleCount: 120,
-    sizeRange: [3, 14],   // más variedad
-    fallSpeed: 0.12,      // lento
-    windPower: 0.2,
-};
